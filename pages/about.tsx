a1e4e4c38d5692333dea74706ac6542db697b118
Shreeji Pedhadiya <shreejilucifer@gmail.com>
2020-10-20 19:04:52 +0530

    about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,85 @@
 import * as React from 'react';
 import Layout from '../src/components/Layout';
+import Paragraph from '../src/components/Paragraph';
+import Title from '../src/components/Title';
 
 interface IAboutPageProps {}
 
 const AboutPage: React.FunctionComponent<IAboutPageProps> = () => {
-	return <Layout overlay navbarType="back"></Layout>;
+	return (
+		<Layout overlay navbarType="back">
+			<Title>About Us</Title>
+			<Paragraph maxHeight="50vh" width="70%">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum at
+				perferendis ipsum error ullam magnam placeat, nostrum provident, nulla
+				nisi quisquam deleniti corporis iste debitis et possimus qui unde est.
+				Ut quos, optio soluta iste voluptatum cum dicta ea necessitatibus magnam
+				delectus debitis provident, deleniti consequatur esse beatae. Quae
+				reprehenderit deserunt minus distinctio error aliquam deleniti nam
+				molestiae, cupiditate delectus. Qui fugit laborum consequuntur earum
+				perferendis quibusdam accusantium eligendi voluptas, nostrum voluptatum,
+				molestiae veritatis. Quam soluta at deserunt itaque. Quis asperiores rem
+				dicta? A molestiae velit excepturi illum laborum facilis. Laboriosam
+				maxime aliquid pariatur blanditiis et aut error ipsa totam, neque,
+				assumenda laudantium? Laborum praesentium reprehenderit facere optio
+				iusto temporibus quibusdam rem eveniet possimus minima, et repudiandae
+				obcaecati harum ex. Quia vel itaque asperiores eum saepe error ducimus
+				odit dolor, dignissimos aspernatur ex! Modi suscipit sint laboriosam
+				alias facere adipisci est, ea vel sunt consequuntur aut voluptas aperiam
+				quo odio. Sint harum vero aliquam quis, saepe provident voluptatum
+				architecto ut, beatae recusandae inventore repellendus maxime, esse id
+				dolores praesentium delectus itaque labore consequuntur aspernatur
+				doloribus at deserunt totam distinctio. Nobis! Non reiciendis
+				dignissimos voluptatum quisquam labore eligendi nesciunt qui modi.
+				Earum, error. Quod tempore explicabo libero at ipsum reiciendis
+				molestias, rerum, illum impedit, ad nobis delectus quidem. Reiciendis,
+				rerum neque! Odit expedita cum iure, nemo placeat earum, vel
+				consequuntur nesciunt, minus amet ipsum deserunt totam praesentium modi
+				ipsa quos possimus at recusandae eligendi. Necessitatibus fugiat nihil
+				debitis aliquid qui iusto? Tempore molestiae voluptate a sequi delectus
+				ab nostrum iusto aut mollitia est id incidunt, dolore porro explicabo?
+				Quasi quam, suscipit provident id, nostrum, quos dignissimos tempora
+				rerum consectetur voluptatem autem. Beatae officiis, facere nostrum
+				recusandae, unde consequuntur qui enim ducimus sit dolorum minima culpa
+				eligendi itaque. Eum pariatur accusamus sed, dicta nemo neque porro quia
+				eveniet. Eaque impedit soluta quas. Nulla deserunt id dolorem voluptates
+				delectus blanditiis molestias, dignissimos non debitis nostrum iure eos
+				quas aliquam magnam ipsum minus suscipit distinctio? Cupiditate ullam
+				quos quisquam quis commodi magnam quod natus! Id nostrum fuga delectus
+				alias libero voluptates accusamus eum, aut facilis veniam? Aliquid,
+				voluptas officia. Atque esse hic aut possimus magnam quisquam commodi
+				quibusdam veritatis libero explicabo, eum autem molestias. Aut ab
+				praesentium maxime voluptate, recusandae ullam inventore non consequatur
+				suscipit magni quia, ipsa eos commodi dolorem libero deleniti
+				repudiandae dicta? Tenetur, ipsam ab. Aliquid quidem repudiandae earum
+				porro expedita. Pariatur nemo at rerum assumenda praesentium odio non
+				excepturi deleniti accusantium, porro quo maxime id saepe enim
+				blanditiis eaque odit, aliquam corrupti tenetur. Adipisci corporis quos
+				quo tempora excepturi perspiciatis! Quos magni quo, ullam mollitia
+				placeat nihil vitae nemo ratione aut ad fuga voluptatibus reiciendis
+				voluptatem consectetur eius amet saepe tempora reprehenderit numquam
+				illo distinctio repellendus atque culpa recusandae. Consequatur?
+				Consequuntur numquam necessitatibus impedit eum. Pariatur perspiciatis
+				deleniti similique et. Voluptatum perferendis in architecto.
+				Exercitationem dolor aspernatur praesentium cum corrupti eaque atque
+				illo architecto tenetur? Eum asperiores dignissimos optio. Ex. Aliquid
+				consequuntur unde fugit aspernatur eos accusamus, laboriosam alias
+				voluptate aliquam. Est sed quaerat ratione quidem perferendis
+				exercitationem optio dolorum tempora corporis odio. Neque eos enim hic
+				praesentium deserunt eligendi? Autem ullam sit hic doloribus voluptatem,
+				dicta porro fugiat corrupti, pariatur ex quod, laborum mollitia possimus
+				consequuntur perspiciatis vero iusto quae saepe libero minus recusandae
+				veniam explicabo aspernatur beatae. Laboriosam? Debitis, et eveniet
+				nobis fugit deserunt officiis veniam nisi repellendus enim temporibus
+				perspiciatis. Aut debitis molestias corporis minus dolorum qui cumque.
+				Delectus doloribus est autem voluptates nostrum, iste itaque nulla?
+				Praesentium, beatae consequuntur cupiditate aliquam ducimus, at magnam
+				ipsam tempora dicta aliquid blanditiis recusandae maxime cum nulla natus
+				incidunt, itaque impedit. Saepe deserunt deleniti laudantium inventore
+				nesciunt maiores quam cupiditate.
+			</Paragraph>
+		</Layout>
+	);
 };
 
 export default AboutPage;
